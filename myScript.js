7694d3157342875f9a939ee17cd37bb4fd7a4be2
Farah Naz <farahnz1@hotmail.com>
2023-06-08 18:06:25 -0400

    changes

--- myScript.js
+++ myScript.js
@@ -16,6 +16,6 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}    
 
 // Caraousal Codes
