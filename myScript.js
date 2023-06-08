7d2318e859d00d4c523d45cfc45a6e40f751cb94
Farah Naz <farahnz1@hotmail.com>
2023-06-08 18:51:01 -0400

    fixed

--- myScript.js
+++ myScript.js
@@ -10,7 +10,7 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
+} 
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
